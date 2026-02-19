01b942409fe90ce277646ee27d4e0ff3a77136a3
Treyvon Pearson <tpearson3@my.waketech.edu>
2026-02-19 14:06:24 -0500

    first

---
+++ check.js
@@ -0,0 +1,13 @@
+const myText = document.getElementById("ageInput")
+const mySubmit = document.getElementById("submitBtn")
+const myResult = document.getElementById("result")
+let age;
+mySubmit.addEventListener("onclick", function () {
+    console.log(age)
+    age = myText.value
+    age = Number(age)
+    if (age < 5) {
+        myResult.textContent = "You are below 5 years old!";
+
+    }
+});
