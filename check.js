f386efca77d80ec16c18f22616093c9e502d2efe
Treyvon <trey2044@outlook.com>
2026-02-19 15:06:34 -0500

    second

--- check.js
+++ check.js
@@ -2,12 +2,11 @@ const myText = document.getElementById("ageInput")
 const mySubmit = document.getElementById("submitBtn")
 const myResult = document.getElementById("result")
 let age;
-mySubmit.addEventListener("onclick", function () {
+mySubmit.addEventListener("click", function () {
     console.log(age)
     age = myText.value
     age = Number(age)
     if (age < 5) {
         myResult.textContent = "You are below 5 years old!";
-
     }
 });
